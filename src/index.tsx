ee50407a3445f69ea82628b5fe13305455c735dd
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-26 08:39:05 +0200

    Added React

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,12 @@
-console.log('src here')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const render = (_App: () => JSX.Element) => {
+    ReactDOM.render(
+        <_App />,
+        document.getElementById('AppContainer')
+    )
+}
+
+render(App)
